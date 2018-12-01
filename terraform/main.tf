de3789d9ea7d063f13c7c1981bf591440132de9d
Chathan Driehuys <chathan@driehuys.com>
2018-11-30 22:51:05 -0500

    Send emails from chathan@driehuys.com
    Temporary fix until a proper email is verified.

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,12 +33,24 @@ data "aws_ami" "server" {
   owners = ["${var.ami_publisher}"]
 }
 
+data "aws_iam_policy_document" "instance-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
 data "aws_route53_zone" "main" {
   name = "${var.domain_name}"
 }
 
 resource "aws_instance" "server" {
   ami             = "${data.aws_ami.server.id}"
+  iam_instance_profile = "${aws_iam_instance_profile.web.name}"
   instance_type   = "${var.server_instance_type}"
   key_name        = "${aws_key_pair.deploy.key_name}"
   security_groups = ["${aws_security_group.server.name}"]
@@ -151,4 +163,45 @@ resource "aws_route53_record" "webserver" {
   ttl     = "60"
   type    = "A"
   zone_id = "${data.aws_route53_zone.main.id}"
+}
+
+################################################################################
+#                                 IAM Profiles                                 #
+################################################################################
+
+resource "aws_iam_instance_profile" "web" {
+  role = "${aws_iam_role.web.name}"
+}
+
+resource "aws_iam_role" "web" {
+  assume_role_policy = "${data.aws_iam_policy_document.instance-assume-role-policy.json}"
+  description        = "Role for ${var.application_name} webservers."
+}
+
+resource "aws_iam_role_policy_attachment" "webserver" {
+  policy_arn = "${aws_iam_policy.webserver.arn}"
+  role       = "${aws_iam_role.web.name}"
+}
+
+resource "aws_iam_policy" "webserver" {
+  description = "Grants webservers access to SES."
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ses:GetSendQuota",
+        "ses:SendEmail",
+        "ses:SendRawEmail"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
 }
